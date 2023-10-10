742f66d48d950b10dd2248be9364d652c7c2b69d
Bogdan Bocioaca <bogdan.bocioaca@gmail.com>
2023-10-10 16:02:09 +0300

    imports

---
+++ app/Policies/DuplicatePolicy.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Duplicate;
+use App\Models\User;
+
+class DuplicatePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return auth()->user()->can('view_duplicates');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Duplicate $duplicate): bool
+    {
+        return auth()->user()->can('view_duplicates');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return auth()->user()->can('manage_duplicates');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Duplicate $duplicate): bool
+    {
+        return auth()->user()->can('manage_duplicates');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Duplicate $duplicate): bool
+    {
+        return auth()->user()->can('manage_duplicates');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Duplicate $duplicate): bool
+    {
+        return auth()->user()->can('manage_duplicates');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Duplicate $duplicate): bool
+    {
+        return auth()->user()->can('manage_duplicates');
+    }
+}
---
+++ app/Policies/ImportExportPolicy.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\ImportExport;
+use App\Models\User;
+
+class ImportExportPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return auth()->user()->can('view_imports');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, ImportExport $importExport): bool
+    {
+        return auth()->user()->can('view_imports');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return auth()->user()->can('manage_imports');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, ImportExport $importExport): bool
+    {
+        return auth()->user()->can('manage_imports');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, ImportExport $importExport): bool
+    {
+        return auth()->user()->can('manage_imports');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, ImportExport $importExport): bool
+    {
+        return auth()->user()->can('manage_imports');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, ImportExport $importExport): bool
+    {
+        return auth()->user()->can('manage_imports');
+    }
+}
