9f17ed5dd60112130952087aea800d25faa300fb
Marius Constantin <marius.constantin@universum.ro>
2023-10-28 10:40:16 +0300

    vue + front api

--- app/Http/Controllers/Api/MapController.php
+++ app/Http/Controllers/Api/MapController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\MapPoint;
+use App\Models\MapPointField;
 use App\Models\MapPointService;
+use App\Models\RecycleMaterial;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -22,5 +24,23 @@ class MapController extends Controller
 			[
 				'points' => MapPoint::getFilteredMapPoints($filters)
 			]);
+	}
+	
+	public function create(Request $request)
+	{
+		$data = $request->all();
+		return response()
+			->json($data);
+	}
+	
+	public function nomenclatures(Request $request)
+	{
+		return response()
+			->json(
+				[
+					'services' => MapPointService::with('pointTypes')->get(),
+					'material_recycling_points' => RecycleMaterial::whereNull('is_wildcard')->get(),
+					'field_types' => MapPointField::all(),
+				]);
 	}
 }
