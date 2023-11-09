5ef093676db0a8ea29b8b13c98272e4a98932c21
Bogdan Bocioaca <bogdan.bocioaca@gmail.com>
2023-11-09 21:00:10 +0200

    issues report

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MapPointsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,7 +12,6 @@ use App\Http\Controllers\MapPointsController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-use App\Livewire\UsersReport;
 
 Route::get('/', function ()
 {
@@ -19,7 +19,7 @@ Route::get('/', function ()
 });
 Route::get('/profile', function ()
 {
-	return view('welcome');
+    return view('welcome');
 });
 use Illuminate\Support\Facades\Route;
 
@@ -30,8 +30,5 @@ Route::prefix('admin')->group(function ()
         Route::get('validate-point/{point_id}', [MapPointsController::class, 'validatePoint'])->name('map-points.validate');
         Route::get('map-view/{point}', [MapPointsController::class, 'mapView'])->name('map_points.map-view');
         Route::get('download-xlsx-example', [MapPointsController::class, 'downloadXlsxExample'])->name('import.xlsx-example');
-        // dd(Route::get('reports/users', UsersReport::class)->name('reports-users'));
-        Route::get('users-reports', UsersReport::class)->name('reports-users');
     });
 });
-
