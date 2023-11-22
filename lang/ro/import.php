c736ddf786beba18d22aabdd5d1c99be1d537401
Bogdan Bocioaca <bogdan.bocioaca@gmail.com>
2023-11-22 12:20:52 +0200

    #9

---
+++ lang/ro/import.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @Author: Bogdan Bocioaca
+ * @Date:   2023-11-22 12:18:24
+ * @Last Modified by:   Bogdan Bocioaca
+ * @Last Modified time: 2023-11-22 12:20:37
+ */
+return [
+    'columns' => [
+        'file'=>'Fisier',
+        'user'=>'Utilizator',
+        'created_at'=>'Creat la',
+        'started_at'=>'Inceput la',
+        'finished_at'=>'Finalizat la',
+        'processed'=>'Adaugate',
+        'failed'=>'Abandonate',
+        'status'=>'Status',
+    ],
+    'status'=>[
+        'pending'=>'In asteptare',
+        'processing' => 'In procesare',
+        'view' => 'Vezi raport',
+    ],
+];
