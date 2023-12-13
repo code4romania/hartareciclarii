44f6ec4b2958990e8906e873fa3bcbda86f91029
Bogdan Bocioaca <bogdan.bocioaca@gmail.com>
2023-12-13 09:07:48 +0200

    42

--- resources/views/filament/resources/puncte-harta-resource/pages/view-punct-harta.blade.php
+++ resources/views/filament/resources/puncte-harta-resource/pages/view-punct-harta.blade.php
@@ -137,6 +137,7 @@
 		            <label for="phone_no" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telefon:</label>
 		            <input value="{{ $record->phone_no }}" type="text" disabled="true" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
 		        </div>
+
 		        <div>
 		            <label for="opening_hours" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Orar:</label>
 		            @if($record->opening_hours)
