8c0fd83d0e6807b13eb2132638c80bea35f4df54
Andrei Ioniță <hi@andrei.io>
2024-09-11 17:10:20 +0100

    wip

--- resources/js/Components/Navigation/Header.vue
+++ resources/js/Components/Navigation/Header.vue
@@ -1,12 +1,12 @@
 <template>
     <Disclosure
         as="header"
-        class="relative z-20 bg-white border-b border-gray-200 shadow md:bg-gray-50"
+        class="relative z-20 bg-white border-b border-gray-200 shadow lg:bg-gray-50"
         v-slot="{ open }"
     >
-        <div class="flex justify-between px-4 py-2 md:px-6 md:py-4 md:gap-6">
+        <div class="flex justify-between px-4 py-3 md:px-6 md:gap-6">
             <Link :href="route('front.map.index')">
-                <Icon icon="logo" class="w-24 h-8 md:w-32 md:h-10 shrink-0" />
+                <Icon icon="logo" class="w-32 h-11 shrink-0" />
             </Link>
 
             <AddPoint
@@ -34,7 +34,7 @@
                 />
 
                 <!-- Profile dropdown -->
-                <Menu v-if="$page.props.auth" as="div" class="relative">
+                <Menu v-if="$page.props.auth" as="div" v-slot="{ close }" class="relative">
                     <MenuButton :as="Button" color="white" :icon="UserIcon" :label="$page.props.auth.full_name" />
 
                     <transition
@@ -48,15 +48,18 @@
                         <MenuItems
                             class="absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                         >
-                            <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                                <Link
-                                    :href="item.href"
-                                    :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']"
-                                    :method="item.method || 'get'"
-                                >
-                                    {{ item.label }}
-                                </Link>
-                            </MenuItem>
+                            <MenuItem
+                                v-for="item in userNavigation"
+                                :key="item.name"
+                                :as="Button"
+                                size="sm"
+                                :href="item.href"
+                                class="block px-4 py-2 text-sm text-gray-700"
+                                @click="close"
+                                :method="item?.method"
+                                :label="item.label"
+                                simple
+                            />
                         </MenuItems>
                     </transition>
                 </Menu>
@@ -67,7 +70,7 @@
             <div class="flex items-center -mr-2 lg:hidden">
                 <!-- Mobile menu button -->
                 <DisclosureButton
-                    class="relative inline-flex items-center justify-center p-1 text-gray-400 bg-white rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
+                    class="relative inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
                 >
                     <span class="absolute -inset-0.5" />
                     <span class="sr-only">Open main menu</span>
@@ -82,13 +85,14 @@
             v-slot="{ close }"
         >
             <div class="px-2 pt-2 pb-3 sm:px-3" v-click-away="close">
-                <div class="pt-2 pb-3 space-y-1">
+                <div class="flex flex-col gap-1 pt-2 pb-3">
                     <DisclosureButton
                         v-if="!dashboard"
                         :label="$t('top_menu.add_point')"
                         :icon="MapPinIcon"
                         @click="openAddPointModal"
                         :as="Button"
+                        class="w-full"
                         simple
                     />
 
@@ -96,14 +100,13 @@
                         v-for="(item, index) in navigation"
                         :key="index"
                         :icon="item.icon"
+                        :label="item.label"
                         :href="item.href"
                         :external="item.external"
-                        @click="() => item.hasOwnProperty('click') && item.click()"
+                        @click="() => close() && item?.click()"
                         :as="Button"
                         simple
-                    >
-                        {{ item.label }}
-                    </DisclosureButton>
+                    />
                 </div>
 
                 <div v-if="$page.props.auth" class="pt-4 pb-3 border-t border-gray-200">
@@ -116,17 +119,18 @@
                             <div class="text-sm font-medium text-gray-500">{{ $page.props.auth.email }}</div>
                         </div>
                     </div>
-                    <div class="mt-3 space-y-1">
+                    <div class="flex flex-col mt-3">
                         <DisclosureButton
                             v-for="(item, index) in userNavigation"
                             :key="index"
-                            :as="Link"
                             :href="item.href"
+                            :label="item.label"
+                            :method="item.method"
                             class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-                            :method="item.method || 'get'"
-                        >
-                            {{ item.label }}
-                        </DisclosureButton>
+                            @click="close"
+                            :as="Button"
+                            simple
+                        />
                     </div>
                 </div>
 
@@ -145,16 +149,10 @@
 
 <script setup>
     import { computed, ref } from 'vue';
-    import { usePage, Link } from '@inertiajs/vue3';
-    import {
-        Disclosure,
-        DisclosureButton,
-        DisclosurePanel,
-        Menu,
-        MenuButton,
-        MenuItem,
-        MenuItems,
-    } from '@headlessui/vue';
+    import { Link } from '@inertiajs/vue3';
+    import { trans } from 'laravel-vue-i18n';
+    import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
+    import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
     import {
         Bars3Icon,
         XMarkIcon,
@@ -168,7 +166,6 @@
     import Icon from '@/Components/Icon.vue';
     import Button from '@/Components/Button.vue';
     import AddPoint from '@/Components/Navigation/AddPoint.vue';
-    import { trans } from 'laravel-vue-i18n';
     import route from '@/Helpers/useRoute.js';
 
     const props = defineProps({
@@ -180,7 +177,9 @@
 
     const isAddPointOpen = ref(false);
 
-    const openAddPointModal = () => (isAddPointOpen.value = true);
+    const openAddPointModal = (close) => {
+        isAddPointOpen.value = true;
+    };
 
     const navigation = computed(() => {
         if (props.dashboard) {
@@ -214,8 +213,4 @@
         { label: trans('profile.settings'), href: route('front.account.settings') },
         { label: trans('profile.logout'), href: route('auth.logout'), method: 'post' },
     ];
-
-    const showLoginModal = ref(true);
-
-    const user = computed(() => usePage().props.user);
 </script>
