ba4543f4effd1348045a4479167df30344a6e8c4
Andrei Ioniță <hi@andrei.io>
2024-09-18 12:10:53 +0100

    wip

--- app/Console/Commands/DeleteAllIndexesCommand.php
+++ app/Console/Commands/DeleteAllIndexesCommand.php
@@ -4,32 +4,23 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
-use App\Models\City;
-use App\Models\Material;
-use App\Models\Point;
 use Illuminate\Console\Command;
 
-class RebuildScoutCommand extends Command
+class DeleteAllIndexesCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'scout:rebuild';
+    protected $signature = 'scout:delete-all-indexes';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Rebuilds the search index for all searchable models';
-
-    protected array $models = [
-        City::class,
-        Material::class,
-        Point::class,
-    ];
+    protected $description = 'Delete all indexes';
 
     /**
      * Execute the console command.
@@ -39,8 +30,7 @@ class RebuildScoutCommand extends Command
         collect(config('scout.typesense.model-settings'))
             ->keys()
             ->each(function (string $model): void {
-                $this->call('scout:flush', ['model' => $model]);
-                $this->call('scout:import', ['model' => $model]);
+                $this->call('scout:delete-index', ['name' => (new $model)->searchableAs()]);
             });
 
         return self::SUCCESS;
