6f9cc07877156464710dbf6fbf9f5fc7d849db55
Lupu Gheorghe <gheorghelupu17@gmail.com>
2025-07-31 15:38:16 +0300

    wip

--- resources/js/Components/Navigation/Header.vue
+++ resources/js/Components/Navigation/Header.vue
@@ -1,7 +1,9 @@
 <template>
     <Disclosure as="header" class="relative bg-white border-b border-gray-200 shadow lg:bg-gray-50" v-slot="{ open }">
         <div class="flex justify-between px-4 py-3 md:px-6 md:gap-6">
-            <Icon icon="logo" class="w-32 h-11 shrink-0" />
+            <a href="https://hartareciclarii.ro/" target="_blank">
+                <Icon icon="logo" class="w-32 h-11 shrink-0" />
+            </a>
 
             <AddPoint
                 v-if="!dashboard"
